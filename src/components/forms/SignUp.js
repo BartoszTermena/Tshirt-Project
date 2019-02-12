b6425df4373d699440b41c3a497050177c66a51e
bartek <Bartek.NNAAN@wp.pl>
2019-02-12 15:43:35 +0100

    user sign up, adding to authentication in firebase, redirecting to main page after sign up

--- src/components/forms/SignUp.js
+++ src/components/forms/SignUp.js
@@ -1,39 +1,59 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { signUp } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
 
-export default class SignUp extends Component {
+class SignUp extends Component {
+    state = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: ''
+    }
+    handleChange = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.signUp(this.state);
+    }
   render() {
+      const { auth, authError } = this.props;
+      if (auth.uid) return <Redirect to="/" />
     return (
         <div className="container-fluid py-5 ">
         <div className="row justify-content-center">
         
             <div className="card bg-light">
             <article className="card-body mx-auto" >
-            <h4 className="card-title mt-3 text-center">Sign Up</h4>
-                <form>
+            <h4 className="card-title mt-3 text-center">You dont have an account yet? Sign Up</h4>
+                <form onSubmit={this.handleSubmit}>
                 <div className="form-group input-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text"> <i className="fa fa-user"></i> </span>
                     </div>
-                    <input name="" className="form-control" placeholder="Choice your username" type="text"/>
+                    <input onChange={this.handleChange} id="firstName" className="form-control" placeholder="First name" type="text"/>
+                </div> 
+                <div className="form-group input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text"> <i className="fa fa-user"></i> </span>
+                    </div>
+                    <input id="lastName" className="form-control" placeholder="Last name" type="text"/>
                 </div> 
                 <div className="form-group input-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
                     </div>
-                    <input name="" className="form-control" placeholder="Email address" type="email"/>
+                    <input onChange={this.handleChange} id="email" className="form-control" placeholder="Email address" type="email"/>
                 </div> 
                 <div className="form-group input-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
                     </div>
-                    <input className="form-control" placeholder="Create password" type="password"/>
-                </div> 
-                <div className="form-group input-group">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
-                    </div>
-                    <input className="form-control" placeholder="Repeat password" type="password"/>
-                </div>                               
+                    <input onChange={this.handleChange} id="password" className="form-control" placeholder="Create password" type="password"/>
+                </div>                                            
                 <div className="form-group">
                     <button type="submit" className="btn btn-primary btn-block"> Create Account  </button>
                 </div>   
@@ -46,3 +66,16 @@ export default class SignUp extends Component {
     )
   }
 }
+const mapStateToProps = (state) => {
+    return {
+      auth: state.firebase.auth,
+      authError: state.auth.authError
+    }
+  }
+  
+  const mapDispatchToProps = (dispatch)=> {
+    return {
+      signUp: (newUser) => dispatch(signUp(newUser))
+    }
+  }
+  export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -15,9 +15,18 @@ const authReducer = (state = initState, action) => {
                 authError: null
             }
         case 'SIGNOUT_SUCCESS':
-            console.log('logout')
             return {
                 ...state
+            }
+        case 'SIGNUP_SUCCESS':
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNUP_ERROR':
+            return {
+                ...state,
+                authError: action.err.message
             }
         default:
             return state;
